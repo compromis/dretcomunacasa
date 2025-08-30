988b0623f679a1d4e34d3ee01d651db97ffe9bfb
Guillem <guillem@disedit.com>
2025-08-30 20:37:39 +0200

    Update nav

--- components/Site/Nav.vue
+++ components/Site/Nav.vue
@@ -1,24 +1,32 @@
 <script setup>
-const { locale, locales } = useI18n()
-const switchLocalePath = useSwitchLocalePath()
 const scrolled = ref(false)
+const section = ref('blue')
+const { $gsap } = useNuxtApp()
 
 onMounted(() => {
   document.addEventListener('scroll', () => {
-    const chart = document.querySelector('.rent-chart')
     const scrollY = window.scrollY
-    const threshold = chart?.offsetTop
-    scrolled.value = scrollY > threshold
+    const shareSection = document.querySelector('#share')?.getBoundingClientRect()
+    const shareTop = shareSection.top + scrollY
+    const shareHeight = shareSection.height || 0
+    const sectionThreshold = shareTop + (shareHeight / 2)
+    scrolled.value = scrollY > 10
+    section.value = scrollY > sectionThreshold ? 'orange' : 'blue'  
   })
+})
+
+watch(section, (newSection) => {
+  const color = newSection === 'orange' ? '#DE5413' : '#1F4C90'
+  document.querySelector('meta[name="theme-color"]').setAttribute('content', color)
 })
 </script>
 
 <template>
-  <div class="campaign-header">
-    <nav :class="['campaign-nav', { scrolled }]">
+  <div :class="['campaign-header', { scrolled, blue: section === 'blue', orange: section === 'orange' }]">
+    <nav class="campaign-nav">
       <div class="flex gap-site w-full md:w-auto justify-between">
         <a href="https://jovespv.org" aria-label="JovesPV - Compromís" class="logo compromis">
-          <SiteLogo />
+          <SiteLogo /> {{ section }}
         </a>
       </div>
       <div class="campaign-name flex text-md font-extrabold leading-[1] text-right">
@@ -59,7 +67,8 @@ onMounted(() => {
   }
 
   &-header {
-    &::before {
+    &::before,
+    &::after {
       content: '';
       position: fixed;
       top: 0;
@@ -67,8 +76,26 @@ onMounted(() => {
       right: 0;
       height: 6rem;
       pointer-events: none;
-      background: linear-gradient(to bottom, rgba(2, 17, 27, .6), rgba(2, 17, 27, 0));
+      background: linear-gradient(to bottom, #1F4C90, #1f4c9000);
       z-index: 1000;
+      transform: translateY(-100%);
+      transition: 1s ease;
+    }
+
+    &::after {
+      background: linear-gradient(to bottom, #DE5413, #de531300);
+    }
+
+    &.scrolled::before {
+      transform: translateY(0);
+    }
+
+    &.orange::after {
+      transform: translateY(0);
+    }
+
+    &.orange::before {
+      transform: translateY(-100%);
     }
   }
 }
